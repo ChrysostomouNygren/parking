28c855c21f90659759a4d1635e4c9f40478e01e3
Katerina <katerina.chrysostomou@hotmail.com>
2025-04-04 23:12:04 +0200

    removed unused import

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String? username;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUser();
+  }
+
+  Future<void> _loadUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      username = prefs.getString('loggedUser');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: LayoutBuilder(builder: (context, constraints) {
+        return Center(
+          child: ConstrainedBox(constraints: const BoxConstraints(maxWidth: 600),
+          child: Padding(padding: const EdgeInsets.all(24),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Välkommen tillbaka $username'),
+            const SizedBox(height: 20,),
+            Text('Starta en parkering'),
+            
+          ],),),),
+        );
+      }),
+    );
   }
 }
