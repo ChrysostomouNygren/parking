f23240e0d2b773f73a08b932ab51511c80eb50b0
Katerina <katerina.chrysostomou@hotmail.com>
2025-04-04 23:30:14 +0200

    added create new person

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,9 +5,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-import 'home_screen.dart';
-
+// Bestämma sig, svenska eller engelska?
 class LoginScreen extends StatefulWidget {
   final VoidCallback onLoginSuccess;
 
@@ -77,6 +75,59 @@ class _LoginScreenState extends State<LoginScreen> {
     ));
   }
 
+  void _showSuccess(String message) {
+    showDialog(context: context, builder: (_) => AlertDialog(
+      title: const Text('Yay! Välkommen!'),
+      content: Text(message),
+      actions: [
+        TextButton(onPressed: () => Navigator.pop(context),
+        child: const Text('OK'),
+        )
+      ],
+    ));
+  }
+
+  void _showRegisterDialog() {
+    final nameController = TextEditingController();
+    final idController = TextEditingController();
+
+    showDialog(context: context, builder: (_) => AlertDialog(
+      title: const Text('Välkommen att registrera ett nytt konto!'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(controller: nameController,
+          decoration: const InputDecoration(labelText: 'Namn'),
+          ),
+          TextField(controller: idController,
+          decoration: const InputDecoration(labelText: 'ID-nummer'),),
+        ],
+      ),
+      actions: [
+        TextButton(onPressed: () => Navigator.pop(context), child: Text('Avbryt'),),
+        ElevatedButton(onPressed: () async {
+          final name = nameController.text.trim();
+          final id = idController.text.trim();
+
+          if (name.isEmpty || id.isEmpty) return;
+
+          final response = await http.post(Uri.parse(Platform.isIOS ? 'http://localhost:8080/persons' : 'http:/10.0.2.2:8080/persons'),
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode({'name': name, 'idNum': id}),
+          );
+          Navigator.pop(context);
+
+          if (response.statusCode == 200) {
+            _showSuccess('Du kan nu logga in på ditt nya konto');
+          } else {
+            _showError('Kunde inte skapa användare');
+          }
+        }, child: const Text('Skapa'),
+        )
+      ],
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,6 +149,7 @@ class _LoginScreenState extends State<LoginScreen> {
               TextFormField(controller: _idController, decoration: InputDecoration(labelText: 'ID number'), obscureText: true,),
               _isLoading ? const CircularProgressIndicator()
               : ElevatedButton(onPressed: _login, child: const Text('Log in')),
+              TextButton(onPressed: _showRegisterDialog, child: Text('Skapa ny användare'))
             ],
                     ),),),),);
           }
